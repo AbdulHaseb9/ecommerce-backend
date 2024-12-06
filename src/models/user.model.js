08f23aa56f4fb5e80ca61c83f2536722fb9500a3
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-12-06 09:51:04 +0500

    changes in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,31 +1,30 @@
 const mongoose = require("mongoose");
 
-
 const userSchema = mongoose.Schema(
     {
-        username: {
-            type: String,
-            require: true,
-            // unique: true,
-            lowercase: true
-        },
-        email: {
-            type: String,
-            require: true,
-            unique: true,
-        },
-        fullname: {
-            type: String,
-            require: true,
-        },
-        password: {
-            type: String,
-            require: true,
-        },
-        avatar: {
-            type: String, // Cloudinary url
-        },
-    }, { timestamps: true }
+    username: {
+        type: String,
+        require: true,
+        // unique: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    fullname: {
+        type: String,
+        require: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    avatar: {
+        type: String, // Cloudinary url
+    },
+}, { timestamps: true }
 )
 
 const User = mongoose.model('User', userSchema)
